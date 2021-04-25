3694cf5834dc4ad969d95d257b871ed2aa59c32e
Adro <acousens@gmail.com>
2021-04-25 01:04:31 -0700

    Add build script to generate date part names

---
+++ build/build-names.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const times = require('../lib/times');
+
+const log = console.log;
+const date = new Date();
+
+const toString = (part, format) => {
+  return date.toLocaleString('default', { [part]: format });
+};
+
+const months = () => {
+  const out = [];
+  times(12, (i) => {
+    date.setMonth(i);
+    for (const style of ['long', 'short', '2-digit']) {
+      out.push({
+        name: toString('month', style),
+        style: style,
+      });
+    }
+  });
+  return out;
+};
+
+const days = () => {
+  const out = [];
+  times(7, (i) => {
+    date.setDate(i + 1);
+    const strings = ['long', 'short'].map((format) => {
+      return toString('weekday', format);
+    });
+    out.push(strings);
+  });
+  return out;
+};
+
+log('');
+log(chalk.yellow('Generating date property names...'));
+
+let m = months();
+let d = days();
+
+const content = `
+  const month = ${JSON.stringify(m)};
+  const weekday = ${JSON.stringify(d)};
+  module.exports = {month, weekday}
+`;
+
+const file = path.resolve(__dirname, '../lib/names.js');
+fs.writeFile(file, content, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  log(' ');
+  log(chalk.green('Generated names. File output to:'));
+  log(chalk.cyanBright(file));
+  log(' ');
+
+  const print = false;
+  if (print) {
+    log(chalk.blue(content));
+  }
+});
