2e759f56882ea5a436ad9f0674548a6af7fb3579
Adro <acousens@gmail.com>
2021-05-17 13:47:39 -0700

    Add h/m/s parsers

--- test/minute.test.js
+++ test/minute.test.js
@@ -1,40 +1,41 @@
 const getValues = require('../lib/util/values');
 const setSequence = require('../lib/util/sequence');
-const { hour } = require('../lib/parsers/hour');
+const { minute } = require('../lib/parsers/minute');
 
 const log = console.log;
 
-describe('hour()', () => {
+describe('minute()', () => {
   describe.each([
     [
       'en-US',
-      'Sun. 1/23/2222 4pm',
+      'Sun. 1/23/2222 4:30pm',
       '2-digit',
-      { name: '4', style: '12-numeric' },
+      { name: '30', style: '2-digit' },
     ],
     ['en-US', '1/23/2222', '2-digit', null],
     [
       'en-US',
       'Tuesday Januray 1, 3030 12:30:04',
       'long',
-      { name: '12', style: '24-2-digit' },
+      { name: '30', style: '2-digit' },
     ],
     ['en-US', 'Tuesday Jan. 1, 3030', 'short', null],
-    ['en-GB', '31-1-20 06PM', '2-digit', { name: '06', style: '12-2-digit' }],
+    ['en-GB', '31-1-20 06:01PM', '2-digit', { name: '01', style: '2-digit' }],
 
     [
       'en-GB',
       'Mon. 14/1/20 3:12:23 PM',
       '2-digit',
-      { name: '3', style: '12-numeric' },
+      { name: '12', style: '2-digit' },
     ],
+    ['et-EE', 'Sun. 31/12/2222 4pm', '2-digit', null],
     [
       'et-EE',
-      'Sun. 31/12/2222 4pm',
+      'Sun. 31/11/3030 4:40pm',
       '2-digit',
-      { name: '4', style: '12-numeric' },
+      { name: '40', style: '2-digit' },
     ],
-    ['et-EE', 'Sun. 31/11 4pm', '2-digit', null],
+    ['et-EE', 'Sun. 31/11 4:40pm', '2-digit', null], // Partial date
     ['ko-KP', 'Sun. 2021. 5. 17.', '2-digit', null],
     ['bs', 'Sunday 17.5.2021.', '2-digit', null],
     ['mn', 'Sunday 2021.05.17', '2-digit', null],
@@ -45,12 +46,8 @@ describe('hour()', () => {
       sequence.filter((obj) => obj.type === 'month')[0].value = {
         style: monthStyle,
       };
-      let match = await hour(values, sequence);
+      let match = await minute(values, sequence);
       expect(match).toEqual(expected);
-      // try {
-      // } catch (error) {
-      //   console.error(error);
-      // }
     });
   });
 });
